5addc3fea5a19a15e7500b34441f0f1a1f7d72a5
Glitch (neat-rocky-airmail) <none>
2021-07-27 11:54:15 +0000

    Upd

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const whid = process.env.DISCORD_WH_ID;
 const alchemytoken = process.env.ALCHEMY_TOKEN;
 
 const address = "0xD6866368Fcbe89bF10aCF948bc5Eb19b01e4dF82"
-const lastBlock = null;// null  9006185; 8987838
+const lastBlock = null;// null  9006185; 8991065
 
 async function init () {
   // const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
@@ -26,13 +26,16 @@ async function init () {
   console.log('thelaurel ready', thelaurel.address);
 }
 
+function displayIssue (issue) {
+  return `<${issue.html_url}> (${issue.title})`
+}
+
 function onTaskEvent (task) {
   const etherscanlink = `https://rinkeby.etherscan.io/tx/` + task.transactionHash;
 
-  const msg = `**New task registered by ${task.organizerData}:**
-${displayLaurelAmount(task.task.amount)} ${task.laurel}
-Url: ${task.gitHubIssue ? task.gitHubIssue.html_url : 'not found'}
-Tx: ${etherscanlink}
+  const msg = `**Task registered by ${task.organizerData} - ${displayLaurelAmount(task.task.amount)} ${task.laurel}**
+Url: ${task.gitHubIssue ? displayIssue(task.gitHubIssue) : 'not found'}
+Tx: <${etherscanlink}>
 `
 console.log('-----onTaskEvent', msg);
   webhook.send(msg)
@@ -52,8 +55,8 @@ function onVoteEvent (data) {
   }
   amount = displayLaurelAmount(amount);
   const msg = `**Vote by ${data.voterData} with ${amount} ${description} (weight ${data.weight}) for option ${data.optionIndex}**
-${data.revertedIndex ? ('Reverted: option ' + data.revertedIndex + '\n') : ''}${data.winnerIndex ? ('WINNER: option ' + data.winnerIndex + '\n') : ''}Tx: ${etherscanlink}
-Task Url: ${data.gitHubIssue ? data.gitHubIssue.html_url : 'not found'}
+${data.revertedIndex ? ('Reverted: option ' + data.revertedIndex + '\n') : ''}${data.winnerIndex ? ('WINNER: option ' + data.winnerIndex + '\n') : ''}Tx: <${etherscanlink}>
+Task Url: ${data.gitHubIssue ? displayIssue(data.gitHubIssue) : 'not found'}
 `
 console.log('-----onVoteEvent', msg);
   webhook.send(msg)
@@ -65,9 +68,9 @@ function onClaimEvent (data) {
   const etherscanlink = `https://rinkeby.etherscan.io/tx/` + data.transactionHash;
 
   const msg = `**Claim ${data.optionIndex} registered by ${data.beneficiaryData}** 
-Proof Url: ${data.optionUrl ? data.optionUrl : 'not found'}
-Tx: ${etherscanlink}
-${data.gitHubIssue ? ("Task: " + data.gitHubIssue.html_url) : 'not found'}
+Proof Url: ${data.optionUrl ? ('<' + data.optionUrl + '>') : 'not found'}
+Tx: <${etherscanlink}>
+${data.gitHubIssue ? ("Task: " + displayIssue(data.gitHubIssue)) : 'not found'}
 `
 console.log('-----onClaimEvent', msg);
   webhook.send(msg)
